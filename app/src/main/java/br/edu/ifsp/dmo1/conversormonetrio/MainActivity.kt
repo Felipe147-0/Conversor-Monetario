91cff0ed0747c8a85de0541ee0a5de972183d683
felipe <fepaganini6@gmail.com>
2024-10-21 11:36:00 -0300

    inicial

---
+++ app/src/main/java/br/edu/ifsp/dmo1/conversormonetrio/MainActivity.kt
@@ -0,0 +1,25 @@
+package br.edu.ifsp.dmo1.conversormonetrio
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+private val DOLLAR_VALUE = 5.50
+private lateinit var inputEditText: EditText
+private lateinit var calculateButton: Button
+private lateinit var outputTextView: TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
