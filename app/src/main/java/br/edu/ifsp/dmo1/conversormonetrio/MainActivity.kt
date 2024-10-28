19c032b62fe200be3d286f17ea949f0e51f9df0b
Felipe <fepaganini6@gmail.com>
2024-10-27 23:39:34 -0300

    criação do XML

--- app/src/main/java/br/edu/ifsp/dmo1/conversormonetrio/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/conversormonetrio/MainActivity.kt
@@ -11,15 +11,45 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
 
-private val DOLLAR_VALUE = 5.50
-private lateinit var inputEditText: EditText
-private lateinit var calculateButton: Button
-private lateinit var outputTextView: TextView
-
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val DOLLAR_VALUE = 5.50
+    private lateinit var inputEditText: EditText
+    private lateinit var calculateButton: Button
+    private lateinit var outputTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findById()
+        configClickListener()
     }
+
+    override fun onClick(v: View) {
+        if (v == calculateButton) {
+            makeConvertion()
+        }
+    }
+
+    private fun findById() {
+        inputEditText = findViewById(R.id.edittext_value)
+        calculateButton = findViewById(R.id.button_calculate)
+        outputTextView = findViewById(R.id.textview_output)
+    }
+
+    private fun configClickListener() {
+        calculateButton.setOnClickListener(this)
+    }
+
+    private fun makeConvertion() {
+        var value = try {
+            inputEditText.text.toString().toDouble()
+        } catch (e: NumberFormatException) {
+            0.0
+        }
+        value = value / DOLLAR_VALUE
+        outputTextView.text = "U$ $value"
+    }
+
 }
